edb01b180b96bf5c738aedcc3fedc59eaa256866
cxuuu <cxuuu@users.noreply.github.com>
2016-07-06 07:53:52 +0000

    fix nginx src check

--- lnmp.sh
+++ lnmp.sh
@@ -119,7 +119,7 @@ check
 # nginx setup
 nginx_setup() {
 cd /usr/local/src
-[ ! -f nginx-1.8.1 ] && wget https://nginx.org/download/nginx-1.8.1.tar.gz
+[ ! -f nginx-1.8.1.tar.gz ] && wget https://nginx.org/download/nginx-1.8.1.tar.gz
 tar -xzvf nginx-1.8.1.tar.gz && cd nginx-1.8.1
 ./configure --prefix=/usr/local/nginx --with-http_realip_module --with-http_ssl_module --with-http_sub_module --with-http_gzip_static_module --with-http_stub_status_module --with-pcre
 make && make install
